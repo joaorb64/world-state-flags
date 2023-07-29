6fb69a90d28c169479aa5522cadc7e556736e5dc
João Ribeiro Bezerra <joaorb64@gmail.com>
2023-07-29 21:12:12 +0100

    Fix readme

--- generate_readme.py
+++ generate_readme.py
@@ -28,9 +28,6 @@ with open("README.md", 'w') as outfile:
             os.popen('find out/'+country["iso2"]+'/ -type f -name \"*.png\"').readlines())
         total_flags = len(country["states"])
 
-        outfile.write(
-            "### "+country["name"]+(' ✔️' if flags_found == total_flags else '')+"\n\n")
-
         states_string = ""
 
         for state in country.get("states"):
@@ -38,15 +35,15 @@ with open("README.md", 'w') as outfile:
                 f'out/{country["iso2"]}/{state.get("state_code")}.png')
 
             if flag_exists:
-                states_string += f"<img src='out/{country['iso2']}/{state.get('state_code')}.png' width='64'/> "
+                states_string += f"<img src='out/{country['iso2']}/{state.get('state_code')}.png' width='32'/> "
 
             states_string += f"""{state.get('state_code')} - {state.get('name')}{' ✔️' if flag_exists else ''}"""
 
-            states_string += "\n"
+            states_string += "\n\n"
 
         outfile.write(
             f'<details>\n'
-            f'<summary>{flags_found}/{total_flags}</summary>\n'
+            f'<summary>###{country["name"]}{(" ✔️" if flags_found == total_flags else "")} ({flags_found}/{total_flags})</summary>\n'
             f'{states_string}'
             f'</details>\n'
         )
